3f6fc1c0659290d0089af768192d72876a782fd9
kotekirkitadze <kotekirkitadze@gmail.com>
2023-05-07 23:19:23 +0400

    add logics for getting products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductComponent } from './components/product/product.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ProductsComponent, ProductComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss'],
+})
+export class ProductsComponent implements OnInit {
+  products$: Observable<any> | undefined;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.products$ = this.http
+      .get('https://kartlos-api.azurewebsites.net/api/products')
+      .pipe(
+        map((e: any) =>
+          e.result.map((e: any) => {
+            return {
+              ...e,
+              descriptionGeo: e.descriptionGeo.split(/• /).slice(1),
+            };
+          })
+        )
+      );
+  }
+}
